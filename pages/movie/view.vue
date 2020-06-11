399e9119476479d757abbbcf44590640e49cdb79
Thitisan <thitisan.th@gmail.com>
2020-06-11 17:05:03 +0700

    add table actor genre production director writer

--- pages/movie/view.vue
+++ pages/movie/view.vue
@@ -11,10 +11,44 @@
       
     <hr>
 
+    <h4>Genre</h4>
+      <ul>
+        <li v-for="movie_genre in movie_genres" :key="movie_genre.genresId">
+          <p>{{movie_genre.genresTitle}}</p>
+        </li>
+      </ul>
+
+    <hr>
+    <h4>Director</h4>
+    <ul>
+      <li v-for="movie_director in movie_directors" :key="movie_director.director_id">
+        <p>{{movie_director.directorFirstname}} {{movie_director.directorLastname}}</p>
+      </li>
+    </ul>
+
+    <hr>
+    <h4>Writer</h4>
+    <ul>
+      <li v-for="movie_writer in movie_writers" :key="movie_writer.writers_id">
+        <p>{{movie_writer.writerFirstname}} {{movie_writer.writerLastname}}</p>
+      </li>
+    </ul>
+
+    <hr>
+
+    <h4>Production</h4>
+    <ul>
+      <li v-for="movie_production in movie_productions" :key="movie_production.pco_id">
+        <p>{{movie_production.pcoName}}</p>
+      </li>
+    </ul>
+
+    <hr>
+
     <h4>Actor</h4>
     <ul>
       <li v-for="actor in actors" :key="actor.actorId">
-        <p>{{actor.actorFirstname}} {{actor.actorLastname}}</p>
+        <p>{{actor.actorFirstname}} {{actor.actorLastname}} Role: {{actor.actorRole}}</p>
       </li>
     </ul>
 
@@ -36,6 +70,10 @@ export default {
             rateId: '',
             rateName:'',
             actors:[],
+            movie_genres:[],
+            movie_productions:[],
+            movie_directors:[],
+            movie_writers:[],
         }
     },
     async created(){
@@ -55,12 +93,31 @@ export default {
         this.rateName = res.data.movie.rateName || ''
       }
       this.getActor()
+      this.getGenres()
+      this.getProduction()
+      this.getDirector()
+      this.getWriter() 
     },
     methods: {
-        async getActor() {
-        let ress = await this.$http.get(`/movie_actor/actorId/${this.$route.query.id}`)
-        this.actors = ress.data.actor
-        console.log('test actor',ress.data)
+      async getActor() {
+        let res = await this.$http.get(`/movie_actor/actorId/${this.$route.query.id}`)
+        this.actors = res.data.actor
+      },
+      async getGenres() {
+        let res = await this.$http.get(`/movie_genres/mgenres/${this.$route.query.id}`)
+        this.movie_genres = res.data.movie_genre
+      },
+      async getProduction() {
+        let res = await this.$http.get(`/movie_production/production/${this.$route.query.id}`)
+        this.movie_productions = res.data.movie_production
+      },
+      async getDirector() {
+        let res = await this.$http.get(`/movie_director/director/${this.$route.query.id}`)
+        this.movie_directors = res.data.movie_director
+      },
+      async getWriter() {
+        let res = await this.$http.get(`/movie_writer/writer/${this.$route.query.id}`)
+        this.movie_writers = res.data.movie_writer
       },
         
     }
