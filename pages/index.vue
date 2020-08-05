066a0b3fa9ab114c9628c0c0f241302d6ef2aeab
Thitisan <thitisan.th@gmail.com>
2020-08-05 14:50:08 +0700

    view

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,58 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        TestApi eiei
-      </h1>
-      <h2 class="subtitle">
-        TestApi Form SQL
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <b-container class="bv-example-row mb-3">
+      <b-row cols="3">
+        <b-col v-for="movie in movies" :key="movie.movieId">
+          <b-card
+            :title="movie.movieTitle"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+            
+          >
+            <b-card-text>
+              {{movie.moviePlot}}
+            </b-card-text>
+
+            <b-button href="#" variant="primary">View Detail</b-button>
+          </b-card>
+        </b-col>
+        
+      </b-row>
+      
+    </b-container>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
   components: {
-    Logo
+    
+  },
+  data(){
+    return{
+      movies:[],
+      rateList:[]
+    }
+  },
+  created(){
+    this.getMovie()
+  },
+  methods:{
+    async getMovie() {
+          let res = await this.$http.get('/movie')
+          let ress = await this.$http.get('/movie/rate')
+          this.movies = res.data.movie
+          this.rateList = ress.data.rate
+          console.log("movie :",this.movies)
+      },
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
